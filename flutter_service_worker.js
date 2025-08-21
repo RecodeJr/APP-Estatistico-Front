03713c9f38ed115eb3bf710731c4a7a4742fea5e
ThiagoSousa81 <ThiagoSousa81@users.noreply.github.com>
2025-08-21 20:24:00 +0000

    deploy: 4ddb17ae4fb946327ecaa38297c4a878d71723c1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "7683baf6a1f7fd7660f245c326b15864",
+const RESOURCES = {"flutter_bootstrap.js": "8168461aac0938fe68200318e6fb3324",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
